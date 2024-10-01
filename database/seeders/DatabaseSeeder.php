fea29903ff792899217790740ae7d949a3fa1305
Diego <lativm@gmail.com>
2024-10-01 19:16:17 -0300

    Revert "Featute:Syncronize Branchs (#1)"
    This reverts commit 7464990fa1e7a5ff687e08ee9cf33b0b7b63dbe8.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,8 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Model::unguard();
-        $this->call(UsersTableSeeder::class);
-        Model::reguard();
+        // $this->call('UsersTableSeeder');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -17,51 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
-});
-
-Route::group(['prefix' => 'api'], function ($router) {
-    Route::post('login', 'Auth\AuthController@login');
-    Route::post('logout', 'Auth\AuthController@logout');
-
-    // ROUTES WITH AUTH
-    Route::group(['middleware' => 'auth:api'], function () {
-        Route::get('me', 'UserController@me');
-        Route::get('users', 'UserController@getUsers');
-        Route::post('refresh', 'Auth\AuthController@refresh');
-
-        // RESTful routes for Client
-        Route::get('clients', 'ClientController@index');          // GET all clients
-        Route::get('clients/{id}', 'ClientController@show');      // GET a single client by ID
-        Route::post('clients', 'ClientController@store');         // POST create a new client
-        Route::put('clients/{id}', 'ClientController@update');    // PUT update a client by ID
-        Route::delete('clients/{id}', 'ClientController@destroy');// DELETE a client (soft delete)
-
-        // RESTful routes for ProductType
-        Route::get('product-types', 'ProductTypeController@index');          // GET all product types
-        Route::get('product-types/{id}', 'ProductTypeController@show');      // GET a single product type by ID
-        Route::post('product-types', 'ProductTypeController@store');         // POST create a new product type
-        Route::put('product-types/{id}', 'ProductTypeController@update');    // PUT update a product type by ID
-        Route::delete('product-types/{id}', 'ProductTypeController@destroy');// DELETE a product type (soft delete)
-
-        // RESTful routes for Product
-        Route::get('products', 'ProductController@index');          // GET all products
-        Route::get('products/{id}', 'ProductController@show');      // GET a single product by ID
-        Route::post('products', 'ProductController@store');         // POST create a new product
-        Route::put('products/{id}', 'ProductController@update');    // PUT update a product by ID
-        Route::delete('products/{id}', 'ProductController@destroy');// DELETE a product (soft delete)
-
-        // RESTful routes for Order
-        Route::get('orders', 'OrderController@index');          // GET all orders
-        Route::get('orders/{id}', 'OrderController@show');      // GET a single order by ID
-        Route::post('orders', 'OrderController@store');         // POST create a new order
-        Route::put('orders/{id}', 'OrderController@update');    // PUT update an order by ID
-        Route::delete('orders/{id}', 'OrderController@destroy');// DELETE an order (soft delete)
-
-        // RESTful routes for OrderProduct (many-to-many relationship)
-        Route::get('order-products', 'OrderProductController@index');                          // GET all order-product relationships
-        Route::get('order-products/{orderId}/{productId}', 'OrderProductController@show');     // GET a single order-product relationship
-        Route::post('order-products', 'OrderProductController@store');                        // POST create a new order-product relationship
-        Route::put('order-products/{orderId}/{productId}', 'OrderProductController@update');   // PUT update an order-product relationship
-        Route::delete('order-products/{orderId}/{productId}', 'OrderProductController@destroy');// DELETE an order-product relationship
-    });
 });
