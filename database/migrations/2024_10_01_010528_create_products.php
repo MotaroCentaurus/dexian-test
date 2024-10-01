8b95cb49a3c182f1577d4455ad0cf987de3330c6
Diego Sousa <diego@diegosousa.com.br>
2024-10-01 15:11:36 -0300

    feat:Create migrations for Clients, ProductType, Products, Orders and OrderProducts

---
+++ database/migrations/2024_10_01_010528_create_products.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->unsignedBigInteger('product_type_id');
+            $table->string('product_name');
+            $table->decimal('price', 8, 2);
+            $table->string('photo')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('product_type_id')->references('product_type_id')->on('product_types')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
